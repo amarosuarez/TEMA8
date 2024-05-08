2ea3d3aaa94406cfc4144b5f0330d5b123f019b3
asuarez <asuarez@106-27.info2>
2024-05-08 09:02:36 +0200

    Ejercicio 2 hecho

--- src/boletin01/ej02/Principal.java
+++ src/boletin01/ej02/Principal.java
@@ -4,18 +4,58 @@ public class Principal {
 
 	public static void main(String[] args) {
 		Hora hora;
+		Hora hora2;
+		Hora hora3;
+		
 		try {
 			hora = new Hora(-22, 15, 30);
+			
 			hora.setMinutos(10);
 			hora.setHora(5);
 			hora.setSegundos(8);
-
-			System.out.println(hora);
-
-		} catch (NegativeMinuteException | NegativeHourException | NegativeSecondException e) {
-			System.out.println(e.getMessage());
-					
-			// e.printStackTrace();
+		} catch (NegativeMinuteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NegativeHourException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NegativeSecondException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			hora2 = new Hora(22, 15, 30);
+			
+			hora2.setMinutos(-10);
+			hora2.setHora(5);
+			hora2.setSegundos(8);
+		} catch (NegativeMinuteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NegativeHourException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NegativeSecondException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			hora3 = new Hora(22, 15, 30);
+			
+			hora3.setMinutos(10);
+			hora3.setHora(5);
+			hora3.setSegundos(-8);
+		} catch (NegativeMinuteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NegativeHourException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NegativeSecondException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 	
